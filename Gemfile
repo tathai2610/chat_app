91797693683aaa9d418b96e51e7935fb57381163
Thai <tathai2610@gmail.com>
2024-03-15 03:29:55 +0700

    Create user, room, message

--- Gemfile
+++ Gemfile
@@ -73,3 +73,7 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem "devise", "~> 4.9"
+
+gem 'slim'
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'rooms#index'
+
+  devise_for :users
+
+  resources :rooms
 end
